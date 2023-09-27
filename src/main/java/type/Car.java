99949ff7a15d4712b6f0708d74cce24af55bc121
WonyuChoi <phillip07290@naver.com>
2023-09-27 22:53:30 +0900

    refactor: move count의 값을 바로 반환하는 메서드 생성

--- src/main/java/type/Car.java
+++ src/main/java/type/Car.java
@@ -4,26 +4,18 @@ public class Car {
 
     private final CarName carName;
     private final MoveCount moveCount;
-    private final StringBuilder carNameOutput;
 
     public Car(CarName carName) {
         this.carName = carName;
         this.moveCount = new MoveCount(1);
-        this.carNameOutput = new StringBuilder().append(carName.getCarName()).append(" : ");
     }
 
-    public MoveCount getMoveCount() {
-        return moveCount;
+    public int getMoveCountValue() {
+        return moveCount.getMoveCount();
     }
 
     public String getCarNameValue() {
         return carName.getCarName();
-    }
-
-    public void moveOrStopByManager(boolean canMove) {
-        if (canMove) {
-            move();
-        }
     }
 
     public void move() {
@@ -31,7 +23,7 @@ public class Car {
     }
 
     public void printCarsMove() {
-        System.out.print(carNameOutput);
+        System.out.printf("%s : ",carName.getCarName());
         System.out.println("-".repeat(moveCount.getMoveCount()));
     }
 
