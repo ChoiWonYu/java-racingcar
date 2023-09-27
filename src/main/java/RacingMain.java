81ecc948d98688f4d2857977b13868eb5b66697a
WonyuChoi <phillip07290@naver.com>
2023-09-27 13:31:26 +0900

    feat : 이동 횟수를 입력받는 기능 추가

--- src/main/java/RacingMain.java
+++ src/main/java/RacingMain.java
@@ -1,11 +1,13 @@
 import java.util.List;
 import type.CarName;
+import type.TryCount;
 import view.InputView;
 
 public class RacingMain {
 
     public static void main(final String[] args) {
         final List<CarName> carNames = InputView.getCarNames();
+        final TryCount tryCount = InputView.getTryCount();
     }
 
 }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 
 import type.CarName;
+import type.TryCount;
 
 public class InputView {
 
@@ -14,7 +15,13 @@ public class InputView {
 
     //아래와 같이 일회성 컬렉션도 일급 컬렉션으로 생성해야 하는지 궁금합니다!
     public static List<CarName> getCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         return Arrays.stream(reader.nextLine().split(","))
             .map(CarName::new).collect(Collectors.toList());
+    }
+
+    public static TryCount getTryCount() {
+        System.out.println("시도할 횟수는 몇회인가요?");
+        return new TryCount(reader.nextInt());
     }
 }
