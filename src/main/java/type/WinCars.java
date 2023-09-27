67936b31f6fe576ed5f5d77bb33a0c833217f0fa
WonyuChoi <phillip07290@naver.com>
2023-09-27 19:31:21 +0900

    feat: 우승한 car의 컬렉션을 일급 컬렉션으로 생성

---
+++ src/main/java/type/WinCars.java
@@ -0,0 +1,20 @@
+package type;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class WinCars {
+
+    private List<Car> winCars;
+
+    public WinCars(List<Car> winCars) {
+        this.winCars = winCars;
+    }
+
+    @Override
+    public String toString() {
+        return winCars.stream()
+            .map(Car::getCarNameValue).collect(Collectors.joining(","));
+    }
+
+}
