d8f72afae74ec445e9e428aa3839878ea481d790
WonyuChoi <phillip07290@naver.com>
2023-09-27 19:35:00 +0900

    feat: 전진 횟수의 최댓값을 구하고 우승자를 구하는 메서드 생성

--- src/main/java/type/CarList.java
+++ src/main/java/type/CarList.java
@@ -1,7 +1,9 @@
 package type;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 public class CarList {
 
@@ -13,5 +15,19 @@ public class CarList {
 
     public void repeatAction(Consumer<Car> action) {
         cars.forEach(action);
+    }
+
+    public WinCars getWinners() {
+        Integer maxValue = getMaxValue();
+        List<Car> winCars = cars.stream()
+            .filter(car -> car.getMoveCount().getMoveCount() == maxValue)
+            .collect(Collectors.toList());
+        return new WinCars(winCars);
+    }
+
+    public Integer getMaxValue() {
+        return cars.stream()
+            .map(Car::getMoveCount).mapToInt(MoveCount::getMoveCount)
+            .max().orElseThrow(NoSuchElementException::new);
     }
 }
