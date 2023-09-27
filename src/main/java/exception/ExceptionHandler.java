661f2666ec24a22fc54b1034009bb964bd9a70d1
WonyuChoi <phillip07290@naver.com>
2023-09-27 21:55:50 +0900

    feat: 예외 처리 전 에러 메세지를 출력할 객체 생성

---
+++ src/main/java/exception/ExceptionHandler.java
@@ -0,0 +1,9 @@
+package exception;
+
+public class ExceptionHandler {
+    public static void printErrorMessage(String errorMessage){
+        System.out.print("[ERROR] ");
+        System.out.println(errorMessage);
+    }
+
+}
