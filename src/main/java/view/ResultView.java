307c84a2e7c4b3aeceec5c13f9255d86913a5740
WonyuChoi <phillip07290@naver.com>
2023-09-27 19:31:48 +0900

    feat: 우승한 car을 출력하는 View 객체

---
+++ src/main/java/view/ResultView.java
@@ -0,0 +1,11 @@
+package view;
+
+import type.WinCars;
+
+public class ResultView {
+
+    public static void printWinners(WinCars winCars){
+        System.out.print(winCars);
+        System.out.println("가 최종 우승했습니다!");
+    }
+}
