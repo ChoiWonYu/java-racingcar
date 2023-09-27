bc2bcc42924d2351ea05373bec9e04db859e59b1
WonyuChoi <phillip07290@naver.com>
2023-09-27 18:56:02 +0900

    feat : car의 이동 거리 원시값 포장 객체 생성

---
+++ src/main/java/type/MoveCount.java
@@ -0,0 +1,20 @@
+package type;
+
+public class MoveCount {
+
+    private int moveCount;
+
+    public MoveCount(int moveCount) {
+        this.moveCount = moveCount;
+    }
+
+
+    public void plusCount() {
+        moveCount++;
+    }
+
+
+    public int getMoveCount() {
+        return moveCount;
+    }
+}
