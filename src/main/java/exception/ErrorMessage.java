526468f797cfaed832ee75de5db8a5c7347dee4b
WonyuChoi <phillip07290@naver.com>
2023-10-03 03:52:29 +0900

    feat: 시도 횟수 유효성 검사에 대한 에러 메세지

--- src/main/java/exception/ErrorMessage.java
+++ src/main/java/exception/ErrorMessage.java
@@ -5,12 +5,13 @@ import domain.TryCount;
 
 public enum ErrorMessage {
     LONGER_THAN_MAX_NAME_LENGTH(String.format("이름은 %d자 이하만 가능합니다.", Name.MAX_LENGTH)),
-    LESS_THAN_MIN_TRY_COUNT(String.format("시도 횟수는 %d 이상이어야 합니다.", TryCount.MIN_TRY_COUNT));
+    LESS_THAN_MIN_TRY_COUNT(String.format("시도 횟수는 %d 이상이어야 합니다.", TryCount.MIN_TRY_COUNT)),
+    WRONG_TYPE_TRY_COUNT("시도 횟수는 정수만 입력 가능합니다.");
 
     private final String message;
 
     ErrorMessage(final String message) {
-        this.message=message;
+        this.message = message;
     }
 
     public static void printErrorMessage(final ErrorMessage handler) {
