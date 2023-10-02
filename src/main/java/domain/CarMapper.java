7ed600c9ca7299be41fae89cf96988d7568d1f39
WonyuChoi <phillip07290@naver.com>
2023-10-03 03:53:46 +0900

    feat: cars 관련 타입 매핑 역할 객체

---
+++ src/main/java/domain/CarMapper.java
@@ -0,0 +1,29 @@
+package domain;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CarMapper {
+
+    public static final String NAME_JOINER = ", ";
+    public static final String NAME_SEPARATOR = ",";
+
+    public static Cars inputToCars(final String separatedCarNames) {
+        String[] carNames = separatedCarNames.split(NAME_SEPARATOR);
+
+        List<Car> targetCars = Arrays.stream(carNames)
+            .map(Car::from)
+            .collect(Collectors.toList());
+
+        return new Cars(targetCars);
+    }
+
+    public static String carsToOutput(final List<Car> targetCars) {
+        List<String> carNames = targetCars.stream()
+            .map(Car::getCarNameValue)
+            .collect(Collectors.toList());
+
+        return String.join(NAME_JOINER, carNames);
+    }
+}
