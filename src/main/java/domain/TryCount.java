d085a832bbfb69bc1c5631919a6f793ac1e266e1
WonyuChoi <phillip07290@naver.com>
2023-10-02 22:32:26 +0900

    refactor: 시도 횟수 크기 유효성 검사

--- src/main/java/domain/TryCount.java
+++ src/main/java/domain/TryCount.java
@@ -1,14 +1,27 @@
 package domain;
 
+import exception.ExceptionHandler;
+
 public class TryCount {
+
+    public static final int MIN_TRY_COUNT = 1;
 
     private final int tryCount;
 
     public TryCount(final int tryCount) {
+        validateSize(tryCount);
         this.tryCount = tryCount;
     }
 
     public int getTryCount() {
         return tryCount;
+    }
+
+    public void validateSize(final int tryCount) {
+        if (tryCount > MIN_TRY_COUNT) {
+            ExceptionHandler.printErrorMessage(
+                String.format("시도 횟수는 %d 이상이어야 합니다.", MIN_TRY_COUNT));
+            throw new IllegalArgumentException();
+        }
     }
 }
