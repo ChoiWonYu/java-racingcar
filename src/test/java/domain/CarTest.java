9840fb90566f75b411a8340d8ca19d3f51e417ed
WonyuChoi <phillip07290@naver.com>
2023-10-03 03:59:54 +0900

    test: 메서드의 반환값에 따라 전진하는지 테스트

--- src/test/java/domain/CarTest.java
+++ src/test/java/domain/CarTest.java
@@ -2,30 +2,46 @@ package domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import service.MovingStrategy;
+import service.StubMovingStrategy;
 
 class CarTest {
 
     private Car car;
+    private MovingStrategy movingStrategy;
 
-    @BeforeEach
-    void init() {
-        car = Car.from("test");
-    }
 
     @Test
-    @DisplayName("움직였을 때 moveCount의 값 증가")
+    @DisplayName("전진 전략의 메서드가 true를 반환하면 전진해야 한다.")
     void when_receive_true_move() {
         //given
+        movingStrategy = new StubMovingStrategy(true);
+        car = Car.of("test", movingStrategy);
         int initialMoveCount = car.getMoveCountValue();
 
         //when
-        car.move();
+        car.moveWhenSatisfiedCondition();
         int resultMoveCount = car.getMoveCountValue();
 
         //then
         assertEquals(initialMoveCount + 1, resultMoveCount);
+    }
+
+    @Test
+    @DisplayName("전진 전략의 메서드가 false를 반환하면 전진하면 안 된다.")
+    void when_receive_false_dont_move() {
+        //given
+        movingStrategy = new StubMovingStrategy(false);
+        car = Car.of("test", movingStrategy);
+
+        //when
+        int initialMoveCount = car.getMoveCountValue();
+        car.moveWhenSatisfiedCondition();
+        int resultMoveCount = car.getMoveCountValue();
+
+        //then
+        assertEquals(initialMoveCount, resultMoveCount);
     }
 }
